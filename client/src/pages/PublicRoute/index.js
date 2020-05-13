fb63a1c82678f84234bb8b001aca33538cd76e30
Jimmy Smith <jasmith.188@gmail.com>
2020-05-13 15:02:49 -0400

    adding cards to public page

--- client/src/pages/PublicRoute/index.js
+++ client/src/pages/PublicRoute/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import "../PublicRoute/public.css";
 
 
 /* This is a very simple component.. it probably doesn't need to be a smart component at this point but you never know what's goingto happen in the future */
@@ -6,19 +7,41 @@ import React from 'react'
 function PublicRoute() {
 
     return (
-        <div className="container">
+        <div>
             <div className="row">
-                <div className="col-sm">
-                </div>
-                <div className="col-sm">
-                    <h3>Example Public Route</h3>
-                    <h6>Anyone can see this content</h6>
-                </div>
-                <div className="col-sm">
+                <div className="col-lg-4">
+                    <div className="chat-popup" id="myForm">
+                        <form action="/action_page.php" class="form-container">
+                            <h1>Chat</h1>
 
+                            <label for="msg"><b>Message</b></label>
+                            <textarea placeholder="Type message.." name="msg" required></textarea>
+
+                            <button type="submit" class="btn">Send</button>
+                            <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                        </form>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="form">
+                        <form class="example" action="action_page.php">
+                            <input type="text" placeholder="Search.." name="search"></input>
+                            <button type="submit"><i class="fa fa-search"></i></button>
+
+                        </form>
+                    </div>
+                </div>
+                <div className="col-lg-4">
+                    <div className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">Itinerary</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
+            
         </div>
+
     )
 }
 
