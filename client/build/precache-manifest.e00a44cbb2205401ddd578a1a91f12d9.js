86f16b6b4cd060fd7d6b2be6e10fe17c3c9b9fd4
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-05-21 13:16:38 -0400

    heroku update

---
+++ client/build/precache-manifest.e00a44cbb2205401ddd578a1a91f12d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76134d3062276cac5657e9353a225823",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3398a64aeb12631383c0",
+    "url": "/static/css/main.f8aa1388.chunk.css"
+  },
+  {
+    "revision": "2216abcf46ca84d19728",
+    "url": "/static/js/2.611e8a2b.chunk.js"
+  },
+  {
+    "revision": "fa24d2988fd9ee29f9cba16979460922",
+    "url": "/static/js/2.611e8a2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3398a64aeb12631383c0",
+    "url": "/static/js/main.f4247808.chunk.js"
+  },
+  {
+    "revision": "20c573aaaa2d44c1c4a3",
+    "url": "/static/js/runtime-main.5908aa69.js"
+  }
+]);
