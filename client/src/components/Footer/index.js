1aff727d2c206a0b98a8de13449fa8a4e2902b69
Jimmy Smith <jasmith.188@gmail.com>
2020-05-21 11:46:08 -0400

    modifying footer with social buttons

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,38 +1,45 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons"
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 import "./index.css"
 
 
 function Footer() {
-    return (
+  return (
     <div className="social-container">
-            <h3 className="social-title">© 2020 Jimmy Smith</h3> 
-            <a
-                href="https://github.com/jasmith188"
-                className="github social">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
-            <a
-                href="https://www.linkedin.com/in/james-smith-286a41112/"
-                className="linkedin social">
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
-          
-            <h3 className="social-title">© 2020 Sterling Etienne</h3>
-            <a
-                href="https://github.com/jasmith188"
-                className="github social">
-                <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
-            <a
-                href="https://www.linkedin.com/in/james-smith-286a41112/"
-                className="linkedin social">
-                <FontAwesomeIcon icon={faLinkedin} size="2x" />
-            </a>
+      <div className="row">
+        <div className="col-6">
+          <span className="social-title">© 2020 Jimmy Smith</span>
+          <br />
+          <a
+            href="https://github.com/jasmith188"
+            className="github social">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/james-smith-286a41112/"
+            className="linkedin social">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
         </div>
-        
- );
+        <div className="col-6">
+          <span className="social-title">© 2020 Sterling Etienne</span>
+          <br />
+          <a
+            href="https://github.com/jasmith188"
+            className="github social">
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/sterling-etienne-4580411a6//"
+            className="linkedin social">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+        </div>
+      </div>
+    </div>
+
+  );
 }
 
 export default Footer;
