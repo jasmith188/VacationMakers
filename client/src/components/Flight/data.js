e0660bc3a502ee848b3f297c6eaca2e42b0ab6a5
Jimmy Smith <jasmith.188@gmail.com>
2020-05-22 09:54:31 -0400

    adding data

--- client/src/components/Flight/data.js
+++ client/src/components/Flight/data.js
@@ -5,71 +5,71 @@ const postData =
          "name": "MCO to ORD", 
          "departure": "09-05-2020",
          "arrival": "09-12-2020",
-         "origin": "Orlando",
-         "destination":"Chicago",
+         "origin": "Chicago",
+         "destination":"Orlando",
          "price": 300
     },{
         "location_id": "355665655786",
         "name": "MCO to HNL", 
         "date": "09-05-20/09-12-20",
-        "origin": "Orlando",
-        "destination":"Honolulu",
+        "origin": "Honolulu",
+        "destination":"Orlando",
         "price": 440
     },{
         "location_id": "355666565786",
         "name": "MCO to IAD",
         "date": "09-05-20/09-12-20",
-        "origin": "Orlando",
-        "destination":"Dulles",
+        "origin": "Washington D.C.",
+        "destination":"Orlando",
         "price": 210
     },{
         "location_id": "355662345786",
         "name": "DAB to MKE",
         "date": "09-05-20/09-12-20",
-        "origin": "Daytona Beach",
-        "destination":"Milwaukee",
+        "origin": "Milwaukee",
+        "destination":"Daytona Beach",
         "price": 390
     },{
         "location_id": "35567865786",
         "name": "DAB to ORD",
         "date": "09-05-20/09-12-20",
-        "origin": "Daytona Beach",
-        "destination":"Chicago",
+        "origin": "Chicago",
+        "destination":"Daytona Beach",
         "price": 460
     },{
         "location_id": "355643565786",
         "name": "DAB to SEA",
         "date": "09-05-20/09-12-20",
-        "origin": "Daytona Beach",
-        "destination":"Seattle",
+        "origin": "Seattle",
+        "destination":"Daytona Beach",
         "price": 420
     },{
         "location_id": "355666765786",
         "name": "DAB to PHX",
         "date": "09-05-20/09-12-20",
-        "origin": "Daytona Beach",
-        "destination":"Phoenix",
+        "origin": "Phoenix",
+        "destination":"Daytona Beach",
         "price": 560
     },{
         "location_id": "355666765786",
         "name": "MIA to ORD",
         "date": "09-05-20/09-12-20",
-        "origin": "Miami",
-        "destination":"Chicago",
+        "origin": "Chicago",
+        "destination":"Miami",
         "price": 560
     },{
         "location_id": "355666765786",
         "name": "MIA to MKE",
         "date": "09-05-20/09-12-20",
-        "origin": "Miami",
-        "destination":"Milwaukee",
+        "origin": "Milwaukee",
+        "destination":"Miami",
         "price": 560
     },{
         "location_id": "355666765786",
         "name": "MIA to IAD",
         "date": "09-05-20/09-12-20",
-        "origin": "Miami",
-        "destination":"Dulles",
+        "origin": "Washington D.C.",
+        "destination":"Miami",
         "price": 560
     }
 ]
